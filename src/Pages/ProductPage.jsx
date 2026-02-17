16dd471135fd45210bbf1c0891e351d82b7aa8a5
Akshar Vijay <aksharvij@gmail.com>
2026-02-17 23:45:20 +1300

    implementing add to cart state in ProductPage

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -1,39 +1,62 @@
-import React, { useEffect, useState } from 'react';
-import { fetchProduct } from '../services/productApi';
-import ProductDetails from '../components/Product/ProductDetails';
+import React, { useEffect, useState } from "react";
+import { fetchProduct } from "../services/productApi";
+import ProductDetails from "../components/Product/ProductDetails";
 
 const ProductPage = () => {
+  const [product, setProduct] = useState(null);
+  const [cartItems, setCartItems] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-    const [product, setProduct] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(true);
-    
-    useEffect(() => {
-        const getProduct = async () => {
-            try {
-                const loadedProduct = await fetchProduct();
-                setProduct(loadedProduct);
-            } catch (err) {
-                console.log(err);
-                setError("Failed to load product data");
-            } finally{ 
-                setLoading(false);
-            }}
-        
-            getProduct();
-        
-        }, [])
-            
+  useEffect(() => {
+    const getProduct = async () => {
+      try {
+        const loadedProduct = await fetchProduct();
+        setProduct(loadedProduct);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load product data");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getProduct();
+  }, []);
+
+  const addToCart = (product, selectedSize) => {
+    const existingItem = cartItems.find(
+      (item) =>
+        item.size.id === selectedSize.id,
+    );
+    if (existingItem) {
+      setCartItems((prev) =>
+        prev.map((item) =>
+          item.size.id === selectedSize.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item,
+        ),
+      );
+    } else {
+      setCartItems((prev) => [
+        ...prev,
+        { product, selectedSize, quantity: 1 },
+    ]);
+    }
+  };
+
   return (
     <div>
-         { error  && <div className="error">{error}</div>}
-              { loading ? <div className="loading">Loading...</div> :
-                <div className="product">
-                    <ProductDetails product={product} />
-                </div>
-              }
+      {error && <div className="error">{error}</div>}
+      {loading ? (
+        <div className="loading">Loading...</div>
+      ) : (
+        <div className="product">
+          <ProductDetails product={product} />
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default ProductPage;
